9fe053c662d43858dfd899401b4a5455ff223d2d
Craig Shaw <craig@craigshaw.com>
2016-07-07 20:42:58 +0100

    Initial commit - Basic application skeleton

---
+++ BargainHunter/Domain.fs
@@ -0,0 +1,77 @@
+﻿module Domain
+
+open System
+open System.IO
+open System.Web
+open FSharp.Data
+
+type HUKDProvider = JsonProvider<"Data/example.json">
+
+type Deal = { Title: string; Link: string; Price: decimal; Listed: DateTime; Category: string }
+
+let printDeals deals =
+    deals 
+    |> Seq.iter (fun i -> 
+        printfn "%s" i.Title
+        printfn "%s" i.Category
+        printfn "%s" i.Link
+        printfn "%O" i.Price
+        printfn "%O\n" i.Listed)
+
+let dateTimeToUnixTime (dt:DateTime) =
+    let epoch = new System.DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)
+    let span = (dt.ToLocalTime() - epoch)
+    int span.TotalSeconds
+
+let unixTimeToDateTime (ut:int) =
+    let epoch = new System.DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)
+    epoch.AddSeconds(float ut).ToLocalTime()
+
+let buildSearchUri (searchString:string) key page = 
+    sprintf 
+        "http://api.hotukdeals.com/rest_api/v2/?key=%s&output=json&forum=deals&exclude_expired=true&results_per_page=30&page=%d&search=%s" 
+        key
+        page 
+        <| HttpUtility.UrlEncode(searchString)
+
+let isNewerThan baseTime timestamp = 
+    DateTime.Compare((unixTimeToDateTime timestamp), baseTime) > 0
+
+let isOlderThan baseTime timestamp = 
+    not <| isNewerThan baseTime timestamp
+
+let (|RelevantDeal|_|) (lastSearchTime:DateTime, deal:HUKDProvider.Item) = 
+    if deal.Timestamp |> isNewerThan lastSearchTime &&
+        deal.Category.Name <> "Gaming" &&
+        match deal.Price with 
+        | Some _ -> true 
+        | None -> false
+    then
+        Some()
+    else
+        None
+
+let getDeals key search lastSearchTime = 
+    let rec loop deals currentPage = 
+        let allDeals = HUKDProvider.Load(buildSearchUri search key currentPage)
+        let relevantDeals = allDeals.Deals.Items 
+                            |> Seq.filter (fun i -> 
+                                            match lastSearchTime, i with
+                                            | RelevantDeal -> true
+                                            | _ -> false)
+                            |> Seq.map (fun i -> 
+                                            {Title = i.Title; 
+                                            Link = i.DealLink; 
+                                            Price = Option.get i.Price; 
+                                            Listed = unixTimeToDateTime i.Timestamp;
+                                            Category = i.Category.Name })
+                            |> Seq.toList
+                            |> List.append deals
+
+        match (allDeals.Deals.Items |> Seq.last, currentPage)  with
+        | (last,_) when last.Timestamp |> isOlderThan lastSearchTime -> relevantDeals
+        | (_,currentPage) when currentPage < (allDeals.TotalResults / 30) -> loop relevantDeals (currentPage + 1) 
+        | _ -> relevantDeals
+
+    loop [] 1
+
---
+++ BargainHunter/Program.fs
@@ -0,0 +1,53 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+open System.IO
+open Domain
+
+let LastRunFile = "last"
+let Version = "0.1.0"
+let AppName = "BargainHunter"
+
+let ApplicationName =
+    sprintf "%s v%s" AppName Version
+
+let getInputParameters argv =
+    if Array.length argv = 2 then
+        Some(argv.[0], argv.[1])
+    else
+        None
+
+let printUsage () = 
+    printfn "Usage:\n%s.exe <HUKD key> <search term>" AppName
+
+let printHeader lastRun search key = 
+    printfn "%s\nLast run: %O\nSearching for '%s' with HUKD key %s\n" ApplicationName lastRun search key
+
+let OneWeekAgo =
+    DateTime.Now.AddDays(-7.0)
+
+let getLastRunTime = 
+    match File.Exists(LastRunFile) with
+    | true -> File.ReadAllText(LastRunFile) |> int |> unixTimeToDateTime
+    | _ -> OneWeekAgo
+
+let findDeals key search =
+    let lastRun = getLastRunTime
+    let currentRun = DateTime.Now
+
+    printHeader lastRun search key
+
+    let deals = getDeals key search lastRun
+
+    printDeals deals
+
+    File.WriteAllText(LastRunFile, string <| dateTimeToUnixTime currentRun)
+
+[<EntryPoint>]
+let main argv = 
+    match getInputParameters argv with
+    | Some(key, search) -> findDeals key search
+    | _ -> printUsage ()
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
