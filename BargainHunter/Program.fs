2b5f2e77522cde7a968c82881e99b5ee29d63841
Craig Shaw <craig@craigshaw.com>
2016-07-07 21:58:08 +0100

    Added crude Slack integration

--- BargainHunter/Program.fs
+++ BargainHunter/Program.fs
@@ -3,6 +3,7 @@
 open System
 open System.IO
 open Domain
+open Slack
 
 let LastRunFile = "last"
 let Version = "0.1.0"
@@ -12,13 +13,13 @@ let ApplicationName =
     sprintf "%s v%s" AppName Version
 
 let getInputParameters argv =
-    if Array.length argv = 2 then
-        Some(argv.[0], argv.[1])
+    if Array.length argv = 3 then
+        Some(argv.[0], argv.[1], argv.[2])
     else
         None
 
 let printUsage () = 
-    printfn "Usage:\n%s.exe <HUKD key> <search term>" AppName
+    printfn "Usage:\n%s.exe <HUKD Key> <Search Term> <Publish Uri>" AppName
 
 let printHeader lastRun search key = 
     printfn "%s\nLast run: %O\nSearching for '%s' with HUKD key %s\n" ApplicationName lastRun search key
@@ -31,7 +32,19 @@ let getLastRunTime =
     | true -> File.ReadAllText(LastRunFile) |> int |> unixTimeToDateTime
     | _ -> OneWeekAgo
 
-let findDeals key search =
+// todo put this behind a mailboxprocessor?
+let publishDeals deals hook =
+    let resp = 
+     deals 
+     |> Seq.map (fun d -> 
+                  sprintf "<%s|%s>\nListed at %O for £%O" d.Link d.Title d.Listed d.Price)
+     |> Seq.fold (fun state deal -> state + deal + "\n\n") ""
+     |> createJsonPayload "Hunter" ":moneybag:"
+     |> postToSlack hook
+
+    printfn "Slack response: %s" resp
+
+let findDeals key search hook =
     let lastRun = getLastRunTime
     let currentRun = DateTime.Now
 
@@ -41,12 +54,14 @@ let findDeals key search =
 
     printDeals deals
 
+    publishDeals deals hook
+
     File.WriteAllText(LastRunFile, string <| dateTimeToUnixTime currentRun)
 
 [<EntryPoint>]
 let main argv = 
     match getInputParameters argv with
-    | Some(key, search) -> findDeals key search
+    | Some(key, search, hook) -> findDeals key search hook
     | _ -> printUsage ()
 
     Console.ReadKey() |> ignore
---
+++ BargainHunter/Slack.fs
@@ -0,0 +1,15 @@
+﻿module Slack
+
+open FSharp.Data
+open FSharp.Data.HttpRequestHeaders
+
+// todo Make this a little more elegant
+
+let postToSlack webHookUri payload =
+    Http.RequestString
+        ( webHookUri, 
+        body = FormValues ["payload", payload])
+
+let createJsonPayload username emoji text =
+    sprintf """{"username":"%s", "text":"%s", "icon_emoji":"%s"}""" username text emoji
+
